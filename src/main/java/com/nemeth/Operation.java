d6348792dc7ff9a206f7e928e103628064e0a7b3
tozogabee <tozogabee@gmail.com>
2017-09-12 21:02:24 +0200

    add Fibonacci

--- src/main/java/com/nemeth/Operation.java
+++ src/main/java/com/nemeth/Operation.java
@@ -1,5 +1,8 @@
 package com.nemeth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by java on 2017.09.12..
  */
@@ -8,5 +11,22 @@ public class Operation {
     public Integer sumNumbers(int a, int b, int c){
 
         return a+b+c;
+    }
+
+    public List<Integer> getFibonacci(int a){
+        int previous0=1;
+        int previous1=1;
+
+        List<Integer> fibonacciList = new ArrayList<Integer>();
+
+        fibonacciList.add(previous0);
+        fibonacciList.add(previous1);
+
+        for (int i=2; i<a; i++){
+            fibonacciList.add(previous0+previous1);
+            int x=previous
+        }
+
+        return fibonacciList;
     }
 }
